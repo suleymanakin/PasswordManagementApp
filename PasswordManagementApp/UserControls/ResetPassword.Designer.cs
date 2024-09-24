35a870536f7498b34e845e7bd57e30e46da9f3b2
Suleyman Akin <slymnakn1@gmail.com>
2024-09-24 17:51:15 +0300

    Password renewal improvements have been made.

--- PasswordManagementApp/UserControls/ResetPassword.Designer.cs
+++ PasswordManagementApp/UserControls/ResetPassword.Designer.cs
@@ -30,7 +30,6 @@
         {
             btnResetPassword = new Button();
             tbxUserEmail = new TextBox();
-            label3 = new Label();
             label1 = new Label();
             label2 = new Label();
             SuspendLayout();
@@ -39,7 +38,7 @@
             // 
             btnResetPassword.Font = new Font("Tajawal", 12F, FontStyle.Bold);
             btnResetPassword.ForeColor = Color.FromArgb(15, 15, 15);
-            btnResetPassword.Location = new Point(347, 388);
+            btnResetPassword.Location = new Point(291, 388);
             btnResetPassword.Name = "btnResetPassword";
             btnResetPassword.Size = new Size(400, 40);
             btnResetPassword.TabIndex = 22;
@@ -52,28 +51,17 @@
             tbxUserEmail.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             tbxUserEmail.Font = new Font("Tajawal", 12F);
             tbxUserEmail.ForeColor = Color.FromArgb(15, 15, 15);
-            tbxUserEmail.Location = new Point(347, 291);
+            tbxUserEmail.Location = new Point(291, 291);
             tbxUserEmail.Name = "tbxUserEmail";
             tbxUserEmail.Size = new Size(400, 35);
             tbxUserEmail.TabIndex = 20;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Tajawal", 12F);
-            label3.ForeColor = Color.FromArgb(15, 15, 15);
-            label3.Location = new Point(236, 294);
-            label3.Name = "label3";
-            label3.Size = new Size(107, 28);
-            label3.TabIndex = 23;
-            label3.Text = "User Email:";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Tajawal", 12F);
             label1.ForeColor = Color.FromArgb(15, 15, 15);
-            label1.Location = new Point(347, 233);
+            label1.Location = new Point(388, 233);
             label1.Name = "label1";
             label1.Size = new Size(207, 28);
             label1.TabIndex = 24;
@@ -84,7 +72,7 @@
             label2.AutoSize = true;
             label2.Font = new Font("Tajawal", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = Color.Red;
-            label2.Location = new Point(347, 340);
+            label2.Location = new Point(272, 340);
             label2.Name = "label2";
             label2.Size = new Size(439, 25);
             label2.TabIndex = 25;
@@ -98,7 +86,6 @@
             Controls.Add(label1);
             Controls.Add(btnResetPassword);
             Controls.Add(tbxUserEmail);
-            Controls.Add(label3);
             Name = "pnlResetPassword";
             Size = new Size(982, 753);
             ResumeLayout(false);
@@ -109,7 +96,6 @@
 
         private Button btnResetPassword;
         private TextBox tbxUserEmail;
-        private Label label3;
         private Label label1;
         private Label label2;
     }
--- PasswordManagementApp/UserControls/ResetPassword.cs
+++ PasswordManagementApp/UserControls/ResetPassword.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PasswordManagementApp.UserControls
@@ -13,19 +7,50 @@ namespace PasswordManagementApp.UserControls
     public partial class pnlResetPassword : UserControl
     {
         AuthService _authService;
+
         public pnlResetPassword()
         {
             InitializeComponent();
             _authService = new AuthService("AIzaSyDJvsRx7pzZw5gcw7gsT_SGl9su8ktJSnI");
+
+            // TextBox olaylarını burada ekleyin
+            tbxUserEmail.GotFocus += new EventHandler(RemoveText);
+            tbxUserEmail.LostFocus += new EventHandler(AddText);
+
+            // Başlangıçta placeholder metnini ayarlayın
+            AddText(null, null);
         }
 
+        // TextBox'a odaklanıldığında placeholder metnini kaldırır
+        private void RemoveText(object sender, EventArgs e)
+        {
+            if (tbxUserEmail.Text == "Enter email")
+            {
+                tbxUserEmail.Text = "";
+                tbxUserEmail.ForeColor = Color.Black;  // Normal metin rengini geri getir
+            }
+        }
+
+        // TextBox odaktan çıktığında eğer boşsa placeholder metnini ekler
+        public void AddText(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbxUserEmail.Text))
+            {
+                tbxUserEmail.Text = "Enter email";  // Placeholder metni
+                tbxUserEmail.ForeColor = Color.DimGray;  // Placeholder rengi gri olsun
+            }
+        }
+
+        // Şifre sıfırlama butonuna tıklama işlemi (isteğe bağlı olarak burada kalabilir)
         private async void btnResetPassword_Click(object sender, EventArgs e)
         {
             string email = tbxUserEmail.Text;
 
-            if (!string.IsNullOrEmpty(email))
+            if (!string.IsNullOrEmpty(email) && email != "Enter email")
             {
                 await _authService.SendPasswordResetEmailAsync(email);
+                tbxUserEmail.Text = string.Empty;  // TextBox'ı temizle
+                AddText(null, null);  // Placeholder metnini geri getir
             }
             else
             {
